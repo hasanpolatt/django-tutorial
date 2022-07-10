6df1f2b425cc6b4533b8e1e2aa4bde282ae7b70a
tygrysco <thatsnotpublic@gmail.com>
2022-07-10 16:13:39 +0300

    added models

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=40)
+    email = models.EmailField()
+    info = models.TextField()
+
+class Category(models.Model):
+    category = models.CharField(max_length=50)
+    description = models.TextField()
+
+class Article(models.Model):
+    title = models.CharField(max_length=75)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="images/")

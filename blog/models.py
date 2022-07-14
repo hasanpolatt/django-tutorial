d91c2efbb7836f6637ee810abc00c35903b88619
tygrysco <thatsnotpublic@gmail.com>
2022-07-14 22:37:47 +0300

    models

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,19 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 class Author(models.Model):
     name = models.CharField(max_length=40)
     email = models.EmailField()
     info = models.TextField()
+
+class Question(models.Model):
+    questionText = models.CharField(max_length=200)
+    pubDate = models.DateTimeField('date published')
+    def __str__(self):
+        return self.questionText
+    def wasPublishedRecently(self):
+        return self.pubDate >= timezone.now() - datetime.timedelta(days=1)
 
 class Category(models.Model):
     category = models.CharField(max_length=50)
